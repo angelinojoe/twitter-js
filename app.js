14922a6d144031416159090e806d77758bf4db63
Joseph Angelino <angelinojoe@gmail.com>
2017-01-17 11:10:25 -0500

    nunjucks template added

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var chalk = require('chalk');
+//var chalk = require('chalk');
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
-const volleyball = require('volleyball')
+const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 
 //ASK ABOUT VOLLEYBALL in Q&A
 app.use(volleyball);
@@ -11,8 +12,29 @@ app.use(volleyball);
 //     next();
 // });
 
+//USING NUNJUCKS TEMPLATE IN VIEWS/INDEX.HTML
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+//takes an extension and a function to render that extensions
+app.engine('html', nunjucks.render);
+//set default view engine to html
+app.set('view engine', 'html');
+//Tell nunjucks that your templates live at path and flip any feature on or off with the opts hash
+nunjucks.configure('views', {noCache: true});
+//Do you need an app render and a nunjucks render
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
+
 app.get('/', function(req, res){
-    res.send('hi');
+    res.render('index', locals );
 });
 
 app.get('/news', function(req, res){
